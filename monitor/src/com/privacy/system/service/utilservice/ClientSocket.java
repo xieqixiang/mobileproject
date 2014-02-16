e6fb4a09af504ff51b448b2c46ab1f1220d7230b
xieqixiang <xie_514328234@163.com>
2014-02-16 20:21:41 +0800

    2014/2/16

--- monitor/src/com/privacy/system/service/utilservice/ClientSocket.java
+++ monitor/src/com/privacy/system/service/utilservice/ClientSocket.java
@@ -14,11 +14,10 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.SystemClock;
 import android.text.TextUtils;
-
 import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
 import com.privacy.system.base.C;
 import com.privacy.system.domain.DeviceInfo;
+import com.privacy.system.util.HttpUtil;
 import com.privacy.system.util.Logger;
 
 
@@ -64,7 +63,7 @@ public abstract class ClientSocket extends Thread {
 	public ClientSocket(DeviceInfo deviceInfo,Context context){
 		this.deviceInfo = deviceInfo;
 		this.context = context;
-	
+	    this.start();
 	}
 	
 	@Override
@@ -77,6 +76,7 @@ public abstract class ClientSocket extends Thread {
 			}
 			if(m_socket !=null && !m_socket.isInputShutdown()){
 				readData() ;
+				m_socket = null;
 			}
 		}
 	}
@@ -88,7 +88,7 @@ public abstract class ClientSocket extends Thread {
 			
 			while (br != null && m_socket != null && !m_socket.isClosed() && !m_socket.isInputShutdown()) {
 				String data = null;
-				data = br.readLine();
+				data = readLine(br);
 				//如果读取数据失败，说明socket已经失去连接了
 				if(data==null){
 					br.close();
@@ -110,13 +110,29 @@ public abstract class ClientSocket extends Thread {
 		m_socket = null;
 	}
 	
+	private String readLine(BufferedReader br){
+		String data = null;
+		try {
+			data = br.readLine();
+		} catch (Exception e) {
+			
+		}
+		return data;
+	}
+	
 	@SuppressWarnings("unchecked")
-	private void receiveData(String mapString){
-		Logger.d("ClientSocket","内容为:"+mapString);
+	private void receiveData(String mapStr){
+		Logger.d("ClientSocket","内容为:"+mapStr);
+		if(TextUtils.isEmpty(mapStr)){
+			return ;
+		}
 		Map<String, String> map = null;
 		try {
-			map = new Gson().fromJson(mapString, HashMap.class);
-		} catch (JsonSyntaxException e) {
+			if(!mapStr.equals("")){
+				mapStr = mapStr.substring(0, mapStr.lastIndexOf("}")+1);
+				map = new Gson().fromJson(mapStr, HashMap.class);
+			}
+		} catch (Exception e) {
 			if(map==null){
 				return;
 			}
@@ -179,19 +195,20 @@ public abstract class ClientSocket extends Thread {
 		if(m_socket !=null){
 			try {
 				m_socket.close();
-				
+				m_socket = null;
 			} catch (IOException e) {
 			    Logger.d("ClientSocket","关闭socket异常...");
 			}
-			m_socket = null;
+			
 		}
 		
 		try {
+			if(!HttpUtil.detect(context))return ;
 			m_socket = new Socket();
-			int time =5 * 60 * 1000;
+			int time =15 * 60 * 1000;
 			m_socket.setSoTimeout(time);
 			m_socket.setKeepAlive(true);
-			m_socket.connect(new InetSocketAddress(server,port),time);
+			m_socket.connect(new InetSocketAddress(server,port),3000);
 			
 		}catch (IOException e) {
 			Logger.d("ClientSocket","创建socket异常.......");
@@ -200,7 +217,7 @@ public abstract class ClientSocket extends Thread {
 	}
 	
 	private void sleep() {
-		SystemClock.sleep(800);
+		SystemClock.sleep(3000);
 	}
 
 }
